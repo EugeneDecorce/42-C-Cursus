9c83d7c63e27a57752ad5438896dd13898876442
EugeneDecorce <102866208+EugeneDecorce@users.noreply.github.com>
2024-05-14 07:05:57 +0200

    Philosophers

---
+++ Philosophers/utils/args_modelling.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   args_modelling.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edecorce <edecorce@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 12:36:39 by edecorce          #+#    #+#             */
+/*   Updated: 2024/04/29 00:33:55 by edecorce         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../philosophers.h"
+
+t_times	proccessed_args(int argc, char **argv)
+{
+	t_times	times;
+
+	times.null = 0;
+	times.die = ft_atoi(argv[1]);
+	times.eat = ft_atoi(argv[2]);
+	times.sleep = ft_atoi(argv[3]);
+	if (argc == 6)
+		times.meals = ft_atoi(argv[4]);
+	else
+		times.meals = -1;
+	return (times);
+}
+
+t_times	null_times(void)
+{
+	t_times	times;
+
+	times.null = 1;
+	return (times);
+}
