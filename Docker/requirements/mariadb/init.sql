48fa603e54b035b02ead2ea44ec39a94b898e71b
EugeneDecorce <102866208+EugeneDecorce@users.noreply.github.com>
2024-08-22 11:00:07 +0200

    Delete Docker directory

--- Docker/requirements/mariadb/init.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER IF NOT EXISTS 'edecorce'@'%' IDENTIFIED BY 'edecorce';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'edecorce'@'%';
-FLUSH PRIVILEGES;
-ALTER USER 'edecorce'@'localhost' IDENTIFIED BY 'edecorce';
