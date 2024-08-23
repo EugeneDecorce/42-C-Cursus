c4aa20b71b5d4372756f48ef3bdf4450db1324a6
EugeneDecorce <102866208+EugeneDecorce@users.noreply.github.com>
2024-08-23 04:15:37 -0500

    Add files via upload

---
+++ Inception/srcs/requirements/nginx/tools/nginx_start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Define SSL certificate and key paths
+CERT_FILE="/etc/ssl/certs/nginx.crt"
+KEY_FILE="/etc/ssl/private/nginx.key"
+
+# Check if the SSL certificate file doesn't exist
+if [ ! -f "$CERT_FILE" ]; then
+	echo "Nginx: Setting up SSL..."
+
+	# Generate the SSL certificate and key
+	openssl req -x509 -nodes -days 365 -newkey rsa:4096 \
+		-keyout "$KEY_FILE" \
+		-out "$CERT_FILE" \
+		-subj "/C=FR/ST=PARIS/L=PARIS/O=42edecorce/CN=edecorce.42.fr"
+
+	# Check if the SSL setup was successful
+	if [ $? -eq 0 ]; then
+		echo "Nginx: SSL is set up successfully!"
+	else
+		echo "Nginx: Failed to set up SSL." >&2
+		exit 1
+	fi
+else
+	echo "Nginx: SSL is already set up."
+fi
+
+# Execute the command passed as arguments to the script
+exec "$@"
